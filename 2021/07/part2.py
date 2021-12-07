29cead355fda5f4028f7dd2b5c9dd7e936277002
Matthias Heinz <heinz.matthias.3@gmail.com>
2021-12-07 08:41:50 +0100

    Add AOC 2021 07.

---
+++ 2021/07/part2.py
@@ -0,0 +1,26 @@
+import os
+from typing import Sequence
+
+
+cur_dir = os.path.dirname(os.path.abspath(__file__))
+
+
+def get_triangular_number(dist: int) -> int:
+    return dist * (dist + 1) // 2
+
+
+def get_fuel_cost(positions: Sequence[int], align_position: int) -> int:
+    return sum([get_triangular_number(abs(x - align_position)) for x in positions])
+
+
+with open(f"{cur_dir}/input") as f:
+    positions = [int(x) for x in f.readline().strip().split(",")]
+
+
+min_pos = min(positions)
+max_pos = max(positions)
+
+align_candidates = list(range(min_pos, max_pos + 1))
+align_costs = [get_fuel_cost(positions, x) for x in align_candidates]
+
+print(min(align_costs))
